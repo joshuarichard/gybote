64e9664de9fd9945957bc9e45ea840ec1c8cfbdf
Joshua Richard <eos.josh.richard@gmail.com>
2015-07-21 20:47:29 -0400

    Made gybote more robust by moving twitter session into a separate method. Previously kept crashing because multiple connections were made at the same time. Also changed the time between tweets from 10 to 5 minutes, and got rid of some excessive debugging. Also fixed what happens if the pointer goes too far, which should never happen anyway.

--- gybote.py
+++ gybote.py
@@ -6,32 +6,34 @@ import logging
 import random
 import datetime
 
-# constants - should make these env vars if deploying to BM
-API_KEY = ''
-API_SECRET = ''
-ACCESS_TOKEN = ''
-ACCESS_SECRET = ''
+# constants - should look into making a settings.py
+API_KEY = '3ay665fsUUBiOJK6xca8b3ile'
+API_SECRET = '8uLWktHANOxU5gcyimL9JE6Ql765DZFBNX6vlBOmG6S643QBBz'
+ACCESS_TOKEN = '3385189491-pVnK0Y16EgQUDIYiX4pka0MfFfqgokYl2l4QZvN'
+ACCESS_SECRET = 'aY0vilDkj07vKjXZTpp6G1NRi8DLvllVd2HmowcE0zncQ'
 
 log = logging.getLogger()
 
-def send_tweet(line):
-    """ Sends a tweet given the line as an argument. """
+def connect():
+    """Initiate connection with twitter."""
 
-    log.info('Connecting to twitter with credentials:')
-    log.info('CONSUMER_KEY %s', API_KEY)
-    log.info('CONSUMER_SECRET %s', API_SECRET)
-    log.info('ACCESS_KEY %s', ACCESS_TOKEN)
-    log.info('ACCESS_SECRET %s', ACCESS_SECRET)
+    # log.info('CONSUMER_KEY %s', API_KEY)        ----
+    # log.info('CONSUMER_SECRET %s', API_SECRET)     |--- un-comment if you'd like,
+    # log.info('ACCESS_KEY %s', ACCESS_TOKEN)        |    just for debugging
+    # log.info('ACCESS_SECRET %s', ACCESS_SECRET) ----
 
     # connect to twitter
     auth = tweepy.OAuthHandler(API_KEY, API_SECRET)
     auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
     api = tweepy.API(auth)
 
-    log.info('Connected to twitter, now sending tweet...')
+    return api
 
-    # send tweet
-    api.update_status(status=line)
+def send_tweet(line, api_con):
+    """ Very simply sends a tweet given the line as an argument. """
+
+    if line != "":
+        api_con.update_status(status=line)
 
 def choose_line():
     """ Chooses a line from a dictionary of Godspeed You! Black Emperor songs. """
@@ -52,7 +54,7 @@ def choose_line():
     num_of_lines = num_of_lines - 1
 
     # pick a random line
-    r_int = random.randint(0, num_of_lines - 1)
+    r_int = random.randint(0, num_of_lines)
 
     # get the line
     pointer = 0
@@ -63,30 +65,32 @@ def choose_line():
                 return line
         # just to be safe or something?
         elif pointer > num_of_lines:
-            return ""
+            line = ""
+            return line
         else:
             pointer += 1
 
 def main():
-    """ Main. """
+    """ Main function. """
     logging.basicConfig(filename='./logs/gybote.log', level=logging.INFO)
-
     log.info('Starting jtb...')
 
-    # work on clean shutdown
+    api_con = connect()
+
+    # work on clean shutdown to get rid of this while True
     while True:
         log.info('Choosing word to tweet...')
         line = choose_line()
 
         log.info('Sending tweet...')
-        send_tweet(line)
-
+        send_tweet(line, api_con)
         log.info('Tweet sent with the line: %s', line)
+
         date_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
         log.info('------------------------------%s------------------------------------', date_time)
 
-        # sleep for 10 minutes
-        time.sleep(600)
+        # sleep for 5 minutes
+        time.sleep(300)
 
 if __name__ == "__main__":
     main()
