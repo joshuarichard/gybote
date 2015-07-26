6fbad462d00acc391b08412cfd18b719db4c3fc3
joshuarichard <eos.josh.richard@gmail.com>
2015-07-25 22:19:42 -0700

    Deleting api credentials after I accidentally pushed them for the 100th time

--- gybote/config.py
+++ gybote/config.py
@@ -1,6 +1,6 @@
-config = { api_key = '3ay665fsUUBiOJK6xca8b3ile'
-           api_secret = '8uLWktHANOxU5gcyimL9JE6Ql765DZFBNX6vlBOmG6S643QBBz'
-           access_token = '3385189491-pVnK0Y16EgQUDIYiX4pka0MfFfqgokYl2l4QZvN'
-           access_secret = 'aY0vilDkj07vKjXZTpp6G1NRi8DLvllVd2HmowcE0zncQ'
+config = { api_key = ''
+           api_secret = ''
+           access_token = ''
+           access_secret = ''
            comment_char = '#'
 }
