7b2cd7d92a476ee2a2684d77f047b3705721e923
Joshua Richard <eos.josh.richard@gmail.com>
2015-08-02 21:01:11 -0700

    Fixed problem with setup.py classifier

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'Topic :: Communications',
-        'License :: OSI Approved :: Apache Software License',
+        'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
